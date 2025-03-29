d03b55ff7790a203b7593af53084cf96a054f210
pernillasterner <psterner@hotmail.com>
2025-03-29 14:52:00 +0100

    Setting up hero sektion

--- index.php
+++ index.php
@@ -3,9 +3,18 @@
 ?>
 
 <main>
-    <section>
-        <h1>Tattoo Artist</h1>
-        <h2>Oskar Sterner</h2>
+    <section class="hero_container">
+      <div class="inner_container">
+        <div class="heading_wrapper">
+          <h1 class="title">Tattoo Artist</h1>
+          <h2 class="name">Oskar Sterner</h2>
+        </div>
+
+        <div class="social-links_wrapper">
+          <a href="https://www.instagram.com/oskar.sterner/" target="_blank">Instagram</a>
+          <a href="">Facebook</a>
+        </div>
+      </div>
     </section>
 </main>
 
